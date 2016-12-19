45844841f3fd1d0bfe3459f3e116ab3cbec04b0d
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-19 22:51:24 +0800

    fundnav utility draft

--- twfundclear.py
+++ twfundclear.py
@@ -17,6 +17,10 @@ __version__  = '0.1'
 __status__   = 'Beta'
 
 import requests
+from requests.adapters import HTTPAdapter
+
+from lxml.html import document_fromstring
+
 from datetime import date
 import logging
 import sys
@@ -102,24 +106,7 @@ def load_fundcode_csv(csv_file=None):
         codename_dict[t_fund_meta['code']] = t_fund_meta
     return codename_dict
 
-def fetch_fundnav_html(year):
-    '''fetch mutual fund nav for specific year and write to a local file
-    with filename fundnav_{fundcode}_{yyyy}.html
-    '''
-    pass
-    
-def load_fundnav_html(year):
-    '''load and parse fundnav_{fundcode}_{yyyy}.html and 
-    return a json object:
-    [
-        [{yyyy-MM-dd}, {nav-value}], 
-        ...
-    ]    
-    '''
-    pass
-
-
-if __name__ == '__main__':
+def test_fundcode():
     filename = 'fund_codename_%s' % date.today().strftime('%Y%m%d')
     fetch_fundcode_csv(filename)
     fundcode = load_fundcode_csv(filename)
@@ -129,3 +116,108 @@ if __name__ == '__main__':
         count += 1
         if count > 10:
             break
+
+def fetch_fundnav_html(fund_code, year, save_path=None):
+    '''fetch mutual fund nav for specific year and 
+    write to a local file if save_path provided
+    return fetched html content
+    '''
+    
+    FUND_NAV_SRC_URL_TEMPLATE = 'https://announce.fundclear.com.tw/MOPSFundWeb/D02_02P.jsp?fundId={fund_id}&beginDate={begin_date}&endDate={end_date}'
+
+    begin_date = date(int(year),1,1).strftime("%Y/%m/%d")
+    if int(year) == date.today().year:
+        end_date = date.today().strftime("%Y/%m/%d")
+    else:
+        end_date = date(int(year),12,31).strftime("%Y/%m/%d")  
+    url = FUND_NAV_SRC_URL_TEMPLATE.format(fund_id=fund_code,
+                              begin_date=begin_date,
+                              end_date=end_date)
+    sys.stdout.write('%s\n' % url)
+    r = requests.get(url)
+    r.raise_for_status()
+    html_content = b''
+    for chunk in r.iter_content(chunk_size=1024):
+        html_content += chunk
+
+    if save_path:
+        with open(save_path,'wb') as fh:
+            fh.write(html_content)
+    
+    return html_content
+    
+def load_fundnav_html(save_path):
+    '''load and parse fundnav_{fundcode}_{yyyy}.html and 
+    return a json object:
+    [
+        [{yyyy-MM-dd}, {nav-value}], 
+        ...
+    ]    
+    '''
+    html_content = None
+    with open(save_path,'rb') as fh:
+        html_content = fh.read()
+
+    html_content = document_fromstring(codecs.decode(html_content,'big5','ignore'))
+    t_tables = html_content.xpath("//table")
+    TABLE_TITLE_INDEX = 4
+    TABLE_NAV_INDEX_START = 5
+    fund_title = t_tables[TABLE_TITLE_INDEX][0][0].text.replace('\r\n','')
+    t_total = len(t_tables)
+    #logging.debug('t_total: ' + str(t_total))
+    t_count = TABLE_NAV_INDEX_START
+    dataset = []
+    
+    while (t_count <= (t_total-1)):
+        if len(t_tables[t_count]) == 2:
+            t_date_list = [t_child.text for t_child in t_tables[t_count][0]]
+            t_value_list = [t_child.text for t_child in t_tables[t_count][1]]
+            for i in range(0,len(t_date_list)):
+                if i != 0:
+                    if (t_date_list[i].strip() != ''):
+                        dataset.append([t_date_list[i],t_value_list[i]])
+                    #else:
+                    #    logging.debug('remove element ('+ str(t_count) + '#' + str(i) + '): ' + str([t_date_list[i],t_value_list[i]]))
+        #else:
+        #    logging.debug('skip table:\n' + etree.tostring(t_tables[t_count]))
+        t_count += 1
+    
+    fund_nav = []
+    t_count = 0
+    while (t_count < len(dataset)):
+        #logging.info('t_count ' + str(t_count))
+        (_,t_value) = dataset[t_count]
+        #logging.debug(str(t_count) + ' ' + str(dataset[t_count]))
+        if (t_value == '--') or (t_value == 'N/A') or (t_value == '.000000'):
+            del dataset[t_count]
+            continue
+        fund_nav.append([dataset[t_count][0],float(dataset[t_count][1])])
+        t_count += 1
+
+    return fund_nav
+    
+    
+def test_fundnav(step=1):
+    fund_code = 'LU0997587166'
+    year = date.today().year
+    save_path = '%s_%s.html' % (fund_code, year)
+    if step == 1:
+        fetch_fundnav_html(fund_code, year, save_path)
+    else:
+        fundnav = load_fundnav_html(save_path)
+        count = 1
+        for entry in fundnav:
+            sys.stdout.write('%s\n' % str(entry))
+            count += 1
+            if count > 31:
+                break
+            
+if __name__ == '__main__':
+    test_fundnav(1)
+    test_fundnav(2)
+    
+    
+    
+    
+    
+    
